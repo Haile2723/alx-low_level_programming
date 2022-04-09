ac64ef9f23ab224c9d02ab2f6440ceeedb098b43
Haile2723 <haile10192019@gmail.com>
2022-04-09 03:59:32 -0700

    fix task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  *main - Prints out the last part of a quote in the standard error.
@@ -7,6 +8,6 @@
  */
 int main(void)
 {
-fwrite(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 58);
+write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 58);
 return (1);
 }
