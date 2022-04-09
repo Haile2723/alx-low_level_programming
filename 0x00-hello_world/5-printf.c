5ce7da9a147cb3bcd3815fb875cfbdb1406e8618
Haile2723 <haile10192019@gmail.com>
2022-04-09 01:55:56 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-  printf( "\"with proper grammer, but the outcome is a piece of arts");
-  return (0);
+printf("\"with proper grammer, but the outcome is a piece of art,");
+return (0);
 }
