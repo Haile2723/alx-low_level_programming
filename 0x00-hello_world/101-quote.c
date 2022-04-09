9de2dec1f3031ba40368aa2dd3d48d94191020c1
Haile2723 <haile10192019@gmail.com>
2022-04-09 03:52:51 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - Prints out the last part of a quote in the standard error.
+ *
+ *Return: 1 if success
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 58);
+return (1);
+}
