5102365a15a93d9cabba72ebb2f7943846404e20
Haile2723 <haile10192019@gmail.com>
2022-04-10 02:47:14 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Print single digit base 10 numbers
+ *Return: Always 0.
+ */
+int main(void)
+{
+int x = 0;
+while (x < 10)
+{
+printf("%d\n", x);
+x++
+}
+return (0);
+}
