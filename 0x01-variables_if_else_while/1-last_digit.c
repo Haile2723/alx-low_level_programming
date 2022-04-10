456fef067108dd7dc8884261894e554165942dbf
Haile2723 <haile10192019@gmail.com>
2022-04-10 01:55:06 -0700

    Task 3

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,11 +15,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if((n % 10) > 5)
 {
-printf("Last digit of %d is %d&n and is greater than 5\n", n);
+printf("Last digit of %d is %d and is greater than 5\n", n);
 }
 else if((n % 10) < 6 && (n % 10) != 0)
 {
-printf("Last digit of %d is %d&n and is less than 6 and not 0\n", n);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n);
 }
 else
 {
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - Print an alphabets in lowercase
+ *Return: Always 0.
+ */
+int main(void)
+{
+char letter;
+for(letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
+}
