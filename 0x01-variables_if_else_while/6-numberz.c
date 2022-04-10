3e3c3d847305c1dfb5dc83149b02507927d3668f
Haile2723 <haile10192019@gmail.com>
2022-04-10 03:18:48 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/6-numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - print single digit base 10 numbers using putchar
+ *Return: Laways 0.
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; numm++)
+{
+putchar((num % 10) + '0');
+}    
+putchar('\n');
+}
