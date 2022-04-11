c516b69d569249d509706dc847d8e4b1cb6c02cf
Haile2723 <haile10192019@gmail.com>
2022-04-11 02:04:04 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - Print all posible combination of base 10 single digit numbers
+ *Return: Always 0.
+ */
+int main(void)
+{
+int num;
+for (num = 0; num <= 9; num++)
+{
+putchar((num % 10) + '0');
+if (num == 9)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
