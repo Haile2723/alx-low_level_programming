c9bae748e92e61cad20174a11e8c790d25695e86
Haile2723 <haile10192019@gmail.com>
2022-04-11 04:10:48 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 int a, b, c;
-for (a = 0; a < 9; a++)
+for (a = 0; a < 8; a++)
 {
-for (b = a + 1; b < 10; b++)
+for (b = a + 1; b < 9; b++)
 {
-for (c = b + 1; c < 11; c++)
+for (c = b + 1; c < 10; c++)
 {
 putchar((a % 10) + '0');
 putchar((b % 10) + '0');
