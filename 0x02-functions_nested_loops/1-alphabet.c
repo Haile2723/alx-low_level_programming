6de0ef195c2730a8fdf32380784f41471082163e
Haile2723 <haile10192019@gmail.com>
2022-04-13 04:11:27 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *File: main.h
+ *Retur: Always 0.
+ */
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar('\n');
+return (0);
+}
