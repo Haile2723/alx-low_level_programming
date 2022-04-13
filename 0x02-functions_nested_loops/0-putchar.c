7b6b7ec92601800cf1fd4cfba0681c92044f193a
Haile2723 <haile10192019@gmail.com>
2022-04-13 03:47:50 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include <main.h>
-#include <stdio.h>
 
 /**
  *main - Print _putchar
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ *File: main.h
+ *Desc: Header file containing declarations for all functions used in this project.
+ */
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void time_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_time_table(int n);
+#endif
