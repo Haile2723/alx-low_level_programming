df76291c0b3e4cc818c6aa6ccb2105885fd3f953
Haile2723 <haile10192019@gmail.com>
2022-04-14 04:42:23 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,16 +7,7 @@
 int main(void)
 {
 int i;
-if (i > 0)
-{
-printf("%d is positive\n", i);
-}
-else if (i < 0)
-{
-printf("%d is negative\n", i);
-}
-else
-{
-printf("%d is zero\n");
+i = 98;
+positive_or_negative(i);
 retun (0);
 }
