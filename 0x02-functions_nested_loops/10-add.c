100a11f0880c1dbf546ebdafb3bb1928e1ee6861
Haile2723 <haile10192019@gmail.com>
2022-04-13 17:07:50 -0700

    All are done at once

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ *add - Add two integers
+ *Return: The result of the addition.
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *print_to_98 - Print all natural number from input to 98
+ *@n: The number to begin counting at.
+ */
+void print_to_98(int n)
+{
+if (n >= 98)
+{
+while (n > 98)
+{
+printf("%d, ", n--);
+}
+printf("%d\n", n);
+}
+else
+{
+while (n < 98)
+{
+printf("%d, ", n++);
+}
+printf("%d\n", n);
+}
+}
