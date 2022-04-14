61a17fe7202ff7ce74ae21203f20e1cb06dafde4
Haile2723 <haile10192019@gmail.com>
2022-04-13 17:24:03 -0700

    Task 9

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 /**
  *add - Add two integers
  *@a: The first integer to be added
- *@b: The second integer to be added 
+ *@b: The second integer to be added
  *Return: The result of the addition.
  */
 int add(int a, int b)
