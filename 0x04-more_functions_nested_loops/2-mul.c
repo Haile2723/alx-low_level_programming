91d9221a2fb233720050fab4c9302ede01901c39
Haile2723 <haile10192019@gmail.com>
2022-04-15 00:45:05 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *mul - Print a multiple of two digits
+ *@a: The firist digit
+ *@b: The second digit
+ *Return: The value of the multiplication
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
