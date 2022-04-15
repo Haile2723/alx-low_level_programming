454921c66f061b65ffb73710da269897aff382c3
Haile2723 <haile10192019@gmail.com>
2022-04-15 00:01:31 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_isupper - Print 1 if it is upper case , 0 otherwise
+ *@c: the letter to be checked.
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains the 0x04 more functions nested loops project tasks
