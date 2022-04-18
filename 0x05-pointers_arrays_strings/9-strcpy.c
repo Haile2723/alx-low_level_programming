8d4cdc8f5be119f7ce47e4b8d3796f49c88b09b5
Haile2723 <haile10192019@gmail.com>
2022-04-18 07:12:52 -0700

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _strcpy - Copy the string pointed to by @src, including the terminal null byte, 
- * to a buffer pointed to by @dest.
+ * _strcpy - Copy the string pointed to by @src, including the terminal null
+ *           byte, to a buffer pointed to by @dest.
  * @dest: A butter to copy the string to
  * @src: The source string to copy.
  * Return: A pointer to the destination string @dest.
