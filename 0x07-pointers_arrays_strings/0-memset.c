23e82dab26763453256c9434071a615fdd7b2c23
Haile2723 <haile10192019@gmail.com>
2022-04-26 01:03:02 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * memset - Fills the first n bytes of a memory area pointed by @s with the @c.
+ * @s: A pointer to the memory area to be filled.
+ * @b: The character to fill the memory area.
+ * @n: The number of bytes to be filled.
+ * Return: A pointer to the filled memory area @s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int index;
+unsigned char *memory = s, value = c;
+for (index = 0; index < n; index++)
+{
+	memory[index] = value;
+}
+return (memory);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifdef "MAIN.H"
+#define "MAIN.H"
+/**
+ * File: main.h
+ * Desc: Header files containing prototypes usedin 0x07-pointers_arrays_strings
+ */
+#include <stddef.h>
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrn(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chassboard(char (a)[8]);
+void print_diagsums(int *a, int size);
+#endif
