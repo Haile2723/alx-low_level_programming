4c99f8057a209bf4de046963edcf7fa9d1973f3b
Haile2723 <haile10192019@gmail.com>
2022-04-27 13:07:06 -0700

    Task 101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *wildcmp - compare two given strings
  *@s1: The firrst string
@@ -6,7 +7,7 @@
  *Return: The difference between string
  */
 int wildcmp(char *s1, char *s2)
-
+{
 while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 {
 s1++;
