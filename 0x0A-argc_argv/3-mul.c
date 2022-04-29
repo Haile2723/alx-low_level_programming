a2db892fc461bd30a3da66becaedddac71a6cc22
Haile2723 <haile10192019@gmail.com>
2022-04-29 07:24:52 -0700

    Fix Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,7 +9,7 @@
 int main(int argc, char *argv[])
 {
 int num1, num2;
-int  prod;
+int mul;
 if (argc != 3)
 {
 printf("Error\n");
@@ -17,7 +17,7 @@ return (1);
 }
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-prod = num1 * num2;
-printf("%d\n", prod);
+mul = num1 * num2;
+printf("%d\n", mul);
 return (0);
 }
