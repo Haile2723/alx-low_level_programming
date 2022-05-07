c55e7a2e9868462bcdd7ed634c75c364e3589dad
Haile2723 <haile10192019@gmail.com>
2022-05-07 04:02:27 -0700

    Task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -7,9 +7,7 @@
  * @ptr: a pointer to a memory previously allocated.
  * @old_size: The size of the allocated space for ptr.
  * @new_size: The new size of new memory block.
- * Return: If new_size == old_size - ptr,
- *         If new_size == 0 and ptr is not NULL - NULL,
- *         Otherwise - a pointer to the reallocated memory block.
+ * Return: pointer to array.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
