754ae0c682a17378b10fd48804e6c032d4908c6f
Haile2723 <haile10192019@gmail.com>
2022-05-09 04:53:15 -0700

    Fix Task 4

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,9 +1,9 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-/**
+/*
  * File: 3-function_like_macro.h
- * Desc: Header file macro named ABS 
+ * Desc: Header file macro named ABS
  */
 
 #define ABS(x) ((x) < 0 ? (-x) : (x))
