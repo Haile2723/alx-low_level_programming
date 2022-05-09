313598574c6f625703b2e4326e67adfb04459443
Haile2723 <haile10192019@gmail.com>
2022-05-09 14:58:33 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,7 +4,7 @@
 
 int _strlen(char *str);
 char *_strcopy(char *dest, char *src);
-
+dog_t *new_dog(char *name, float age, char *owner);
 /**
  * _strlen - Finds the length of a string
  * @str: The string to be measured.
