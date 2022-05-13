c224031297c5c498d54b40c548a6d279d86d7211
Haile2723 <haile10192019@gmail.com>
2022-05-13 01:23:58 -0700

    Task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,5 @@
 #include "3-calc.h"
 #include <stdlib.h>
-
 /**
  * get_op_func - Selects the correct function to perform the operation
  * @s: The operator passed as an argument
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,10 +1,4 @@
 #include "3-calc.h"
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-
 /**
  * op_add - Returns the addition of two argument
  * @a: The first argument
