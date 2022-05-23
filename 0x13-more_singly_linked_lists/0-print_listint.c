d153f9b82c0f432dd59dd930f2ad6f1b6e242524
Haile2723 <haile10192019@gmail.com>
2022-05-23 12:03:06 -0700

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,10 +2,8 @@
  * File: 0-print_listint.c
  * Auth: Haile2723
  */
-
-#include <stdio.h>
 #include "lists.h"
-
+#include <stdio.h>
 /**
  * print_listint - Prints all the elements of a listint_t
  * @h: A pointer to the head of the list_t
