2c36ac4e1508a6fbeeae3ced7ae1a3d4e4f38971
Haile2723 <haile10192019@gmail.com>
2022-05-26 19:41:09 -0700

    Task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+/*
+ * File: 5-flip_bits.c
+ * Auth: Haile2723
+ *
+ */
+#include ""main.h"
+/**
+ * flip_bits - Count the no of bits needed to be flipped
+ * @n: The number.
+ * @m: The number to flip n to.
+ * Return: The necessary number of bit to flip to get from n to m
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int xor = n ^m, bits = 0;
+while (xor > 0)
+{
+bits += (xor & 1);
+xor >>= 1;
+}
+return (bits);
+}
