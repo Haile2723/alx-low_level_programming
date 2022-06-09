876d112a1565b4a5932c6cfd011e898f0d08961d
Haile2723 <haile10192019@gmail.com>
2022-06-08 22:40:54 -0700

    Task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+/*
+ * File: 2-str_concat.c
+ * Auth: Haile2723
+ */
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - Concatenates two strings.
+ * @s1: The string to be concatenated upon.
+ * @s2: The string to be concatenated to s1.
+ * Return: If it fails - NULL, Otherwise - a pointer to newly-allocated space
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+char *concat_str;
+int index, concat_index = 0, len = 0;
+
+if (s1 == NULL)
+s1 = "";
+
+if (s2 == NULL)
+s2 = "";
+
+for (index = 0; s1[index] || s2[index]; index++)
+len++;
+
+concat_str = malloc(sizeof(char) * len);
+
+if (concat_str == NULL)
+return (NULL);
+
+for (index = 0; s1[index]; index++)
+concat_str[concat_index++] = s1[index];
+
+for (index = 0; s2[index]; index++)
+concat_str[concat_index++] = s2[index];
+
+return (concat_str);
+}
