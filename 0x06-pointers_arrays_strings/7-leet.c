5dfe6229121fa5ac4763d2e4434d08dcc609303f
Haile2723 <haile10192019@gmail.com>
2022-06-09 04:42:51 -0700

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+/*
+ * File: 7-leet.c
+ * Auth: Haile2723
+ */
+
+#include "main.h"
+
+/**
+ * leet - Encodes a string to 1337
+ * @str: The string to be encoded.
+ * Return: A pointer to the encoded string.
+ */
+char *leet(char *str)
+{
+int indx1 = 0, indx2;
+char leet[8] = {'0', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+while (str[indx])
+{
+for (indx2 = 0; indx2 <= 7; indx2++)
+{
+if (str[index] == leet[indx2] || str[indx1] - 32 == leet[indx2])
+str[indx1] = indx2 + '0';
+}
+
+indx1++;
+}
+
+return (str);
+}
