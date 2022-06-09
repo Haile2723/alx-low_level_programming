3990d400c22a970b58ad33d8fd62f0e544584f89
Haile2723 <haile10192019@gmail.com>
2022-06-09 02:53:23 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,26 @@
-#include "main.h"
-/**
- * strcat - Concatenates the string pointed to @src
- * @dest: A pointer the string to be Concatenated
- * @src: The source string to be appended to @dest
- * Return: A pointer to the destination string @dest
+/*
+ * File: 0-strcat.c
+ * Auth: Haile2723
  */
-char *_strcat(char *dest, char *src)
+
+#include "main.h"
+
+/**
+ * strcat - Concatenates the string pointed to @src.
+ * @dest: A pointer to the string to be concatenated upon
+ * @src: The source string to be appended to @dest.
+ * Return: A pointer to  the destination string to @dest.
+ */
+
+char *_strcat(char *dest, const char *src)
 {
-int index = 0, int dest_len = 0;
+int index = 0, dest_len = 0;
+
 while (dest[index++])
-{
 dest_len++;
-}
+
 for (index = 0; src[index]; index++)
-{
 dest[dest_len++] = src[index];
-}
+
 return (dest);
 }
